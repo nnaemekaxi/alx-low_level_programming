e7376ede70f26d56aaa4cac9673cecd6c02d3dd0
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-08 13:37:16 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = o;
+while (c <= 9)
+{
+putchar(48 + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+putchar('\n');
+return (0)
+}
