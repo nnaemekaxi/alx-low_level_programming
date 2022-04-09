1ac4a305595ec6fdc6d71b095a9ce93721679e28
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-09 11:37:01 -0700

    combss

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	int aa;
+	int ab;
+
+	int b;
+	int ba;
+	int bb;
+
+	while (a <= 98)
+	{
+		aa = (a / 10 + '0');
+		ab = (a % 10 + '0');
+	b = 0;
+	while (b <= 99)
+	{
+		ba = (b / 10 + '0');
+		bb = (b % 10 + '0');
+
+	if (a < b)
+	{
+		putchar(aa);
+		putchar(ab);
+		putchar(' ');
+		putchar(ba);
+		putchar(bb);
+
+	if (a != 98)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	b++;
+	}
+	a++;
+	}
+	putchar('\n')
+	return (0);
+}
