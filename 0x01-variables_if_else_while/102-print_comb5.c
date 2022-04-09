78fadb0c47b1e8d4cfbf89c1d1b3661a1130705d
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-09 11:41:21 -0700

    combss

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - Write a program that prints all possible combinations of two two-digit numbers.
+ * main - Write a program that prints
+ * all possible combinations of two two-digit numbers.
  *
  * Return: Always 0 (Success)
  */
