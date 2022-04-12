29005eb898e611dadb8f871c7fd25a0fa2dea5ea
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-12 06:19:06 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - a file that prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char c = '_';
+char d = 'p';
+char e = 'u';
+char f = 't';
+char g = 'c';
+char h = 'h';
+char i = 'a';
+char j = 'r';
+
+putchar(c);
+putchar(d);
+putchar(e);
+putchar(f);
+putchar(g);
+putchar(h);
+putchar(i);
+putchar(j);
+
+return (0);
+}
+
