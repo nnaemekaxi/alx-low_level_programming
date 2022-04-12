647e52541c76f8772be835ad470c27a2dfc7a761
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-12 10:04:46 -0700

    alph

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - check the code
  *
  * Return: Always 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char a = 'a';
 
 	for (a = 'a'; a <= 'z'; a++)
 	{
-		putchar(a);
+		_putchar(a);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
