7a7b5f80c1efec3fb9b57253412130a3c0758ed7
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-12 15:16:35 -0700

    c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lower case c
+ *
+ * Return: 1
+ */
+
+int _islower(int c)
+{
+	int c;
+	char l;
+	int l;
+
+	l = getchar();
+	if (l == 99)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
+	return (0);
+}
