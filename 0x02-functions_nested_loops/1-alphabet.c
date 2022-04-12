08823ba0f6141cc0daa621cc7381d4f60360636d
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-12 08:31:27 -0700

    alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		print_alphabet(a);
+		return (0);
+	}
+}
