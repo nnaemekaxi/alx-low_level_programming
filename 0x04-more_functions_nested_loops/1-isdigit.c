26e5d9e5ec75dc59a0eaae6bcc8dc7b074b4688c
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-14 08:42:23 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+
+}
