f5d0d20216088873c1d3dc2dd9409d8d7873a350
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-20 13:52:40 -0700

    more

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,7 @@
 */
 
 void puts_half(char *str)
-{ 
+{
 int len, n, i;
 
 	len = 0;
@@ -15,7 +15,7 @@ while (str[len] != '\0')
 {
 len++;
 }
-  
+
 if (len % 2 == 0)
 {
 for (i = len / 2; str[i] != '\0'; i++)
