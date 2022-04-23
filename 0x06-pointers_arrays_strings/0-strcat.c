191430fc785069f356d372d9a2ed1c2998f66181
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-23 11:50:02 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,16 @@ char *_strcat(char *dest, char *src)
 	int y;
 
 	x = 0;
-	while (dest[x] != 0)
+	while (dest[x] != '\0')
 	{
 		x++;
 	}
 	y = 0;
-	while (src[y] != 0)
+	while (src[y] != '\0')
 	{
 		dest[x] = src[y];
 		x++;
 		y++;
 	}
-	dest[x] = '\0';
 	return (dest);
 }
