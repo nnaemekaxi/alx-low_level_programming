35d42a145a7774136d0b573507e8763655c967d7
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-23 13:34:10 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ * @str - string to encode
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	int x;
+	int y;
+	char c[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		for (y = 0; c[y] != '\0'; y++)
+		{
+			if (str[x] == c[y])
+			{
+				str[x] = n[y];
+			}
+		}
+	}
+	return (str);
+}
