06ac140b3be68d85e7c9f65d6002aaae7e1b67ed
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-23 14:24:42 -0700

    print buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: size of buffer
+ *
+ */
+void print_buffer(char *b, int size)
+{
+	int x;
+	int y;
+	int z;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (x = 0; x < size; x += 10)
+		{
+			printf("%.8x:", x);
+			for (y = x; y < x + 10; y++)
+			{
+				if (y % 2 == 0)
+					printf(" ");
+				if (y < size)
+					printf("%.2x", *(b + y));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (z = x; z < x + 10; z++)
+			{
+				if (z >= size)
+					break;
+				if (*(b + z) < 32 || *(b + z) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + z));
+			}
+			printf("\n");
+		}
+	}
+}
