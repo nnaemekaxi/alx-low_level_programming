8ce8b3091cafd6051861a44af9770b22981d2d65
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-23 13:15:13 -0700

    corrected

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,30 @@
 #include "main.h"
-
 /**
- * *_strcat - function commute srtings
- * @dest: param pointer to a char
- * @src: param pointer to a char
- * Return: return value of dest
+ * _strcat - concatenates two strings
+ * @dest: input parameter 'string'
+ * @src: input parameter 'string'
+ *
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int x;
+	int y;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
+	x = 0;
+	while (dest[x] != '\0')
 	{
-		i++;
+		x++;
 	}
-	while (src[j] != '\0')
+	y = 0;
+	while (src[y] != '\0')
 	{
-		dest[i] = src[j];
-		j++;
-		i++;
+		dest[x] = src[y];
+		y++;
+		x++;
 	}
-
-	dest[i] = '\0';
+	dest[x] = '\0';
 	return (dest);
 }
+
