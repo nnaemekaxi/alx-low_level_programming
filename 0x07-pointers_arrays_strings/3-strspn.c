cb6427e23b931564855213c21b87b815cfa06416
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-26 12:40:42 -0700

    strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+	int y;
+
+	while (*s)
+	{
+		y = 0;
+		while (accept[y])
+		{
+			if (*s == accept[y])
+			{
+				x++;
+				break;
+			}
+			else if (accept[y + 1] == '\0')
+				return (x);
+				y++;
+		}
+		s++;
+	}
+}
