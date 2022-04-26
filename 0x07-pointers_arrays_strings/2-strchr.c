d4baee3da0785be48a9e12393f2b557389a6f93f
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-26 12:27:45 -0700

    string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+
+	while (s[x] >= '\0')
+	{
+		if (s[x] == c)
+			return (&s[x]);
+			x++;
+	}
+	return (0);
+}
