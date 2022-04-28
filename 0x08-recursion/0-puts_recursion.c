6d3f6fa10d3e0bb7405d3537342d5d2c59dbc2ff
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-04-28 07:45:24 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function prints
+ * _puts_recursion - function prints strings
+ * @s: string to be inserted
  *
  * Return: nothing
  */
 
 void _puts_recursion(char *s)
 {
-    puts(s);
-    putchar("\n");
+	if (*(s) != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*(s) == '\0')
+		_putchar('\n');
 }
