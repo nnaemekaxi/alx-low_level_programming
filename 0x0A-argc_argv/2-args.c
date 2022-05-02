718a19a21c38df18197dbc908fe92b54ee8b649f
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-05-02 13:50:00 -0700

    args2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the function.
+ * @argc: is argument counter to an int.
+ * @argv: is an agument value determiner for char.
+ *
+ * Return: Returns always success.
+**/
+
+int main(int argc, char *argv[])
+{
+	int x;
+	for (x = 0; x < argc; x++)
+		printf("%s\n", argv[x]);
+
+	return (0);
+
+}
