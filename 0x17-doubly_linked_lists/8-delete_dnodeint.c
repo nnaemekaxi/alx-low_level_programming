bdb70573aa6c8d5f0a02a285aa6d68b5d1013693
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-07-07 13:14:58 -0700

    delete node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (x = 0; temp != NULL && i != index; x++)
+	for (x = 0; temp != NULL && x != index; x++)
 	{
 		prev = temp;
 		temp = temp->next;
